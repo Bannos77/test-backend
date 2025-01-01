5832f5b762e557b95c25c1e8a27e43c4437e4362
Bannos77 <159015134+Bannos77@users.noreply.github.com>
2025-01-01 15:32:10 +0100

    callbackurl_update

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'https://ap-ecc-express.onrender.com/auth/google/callback'
+  callbackURL: 'https://back-end-rrmj.onrender.com/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const email = profile.emails[0].value;
